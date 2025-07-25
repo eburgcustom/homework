a24b97edfba9d6374ad4dffbfc593827eb5f9973
Ildar <gif8686@gmail.com>
2025-07-26 01:36:01 +0500

    создан файл widget.py и написана функция mask_account_card

---
+++ src/widget.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from .masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(data: str) -> str:
+    """ Маскирует номер карты/счёта в переданной строке. """
+    parts = data.split()
+    if not parts:
+        raise ValueError("Пустая строка")
+
+    # Определяем тип (карта или счёт)
+    if parts[0] == "Счет":
+        account_number = parts[-1]
+        masked_number = get_mask_account(account_number)
+        return f"Счет {masked_number}"
+    else:
+        card_number = parts[-1]
+        masked_number = get_mask_card_number(card_number)
+        return ' '.join(parts[:-1] + [masked_number])
+
